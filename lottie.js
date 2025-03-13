0f86a826d67ab3d4b7a4e58271cde7b8daa0ddbd
yseom <yseom86@gmail.com>
2025-03-13 13:28:45 +0900

    Add files via upload

---
+++ lottie.js
@@ -0,0 +1,7 @@
+let anmation = bodymovin.loadAnimation({    
+    container : document.getElementById('lottie'),    
+    renderer : 'svg',    
+    loop : true,   
+    autoplay : true,   
+    path : '/moweb/js/dsa/web_graphic.json', 
+   });
